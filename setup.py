c854e6f5474a302f700c004454d7b3fa6894b6cc
pluto <41194308+sparsh9012@users.noreply.github.com>
2019-04-17 22:12:29 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mykit",
+    version="0.0.1",
+    author="sparsh agarwal",
+    author_email="sp.agarwal13@gmail.com",
+    description="my useful function package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sparsh9012/mykit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
